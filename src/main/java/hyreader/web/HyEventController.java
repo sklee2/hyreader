ce0f900611180c788cf0a3764885a162615c5ef0
sklee2 <sklee2@me.com>
2015-08-30 23:11:41 +0900

    view add

--- src/main/java/hyreader/web/HyEventController.java
+++ src/main/java/hyreader/web/HyEventController.java
@@ -1,15 +1,34 @@
 package hyreader.web;
 
+import hyreader.domain.HyEvent;
+import hyreader.service.HyEventService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import java.util.List;
 
 /**
  * Created by HYU on 2015-08-20.
  */
 @Controller
+@RequestMapping("hyEvents")
 public class HyEventController {
-    @RequestMapping("/")
+    @Autowired
+    HyEventService hyEventService;
+
+    @RequestMapping("/index")
     String index(){
         return "index";
+    }
+
+    @RequestMapping(method = RequestMethod.GET)
+    String list(Model model) {
+        List<HyEvent> hyEvents = hyEventService.findAll();
+        model.addAttribute("hyEvents", hyEvents);
+        return "hyEvents/list";
+
     }
 }
